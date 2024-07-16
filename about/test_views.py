360f5299a3fae1e16b659b168e34f9bfccea4b82
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-07-16 09:06:02 +0000

    Add test for form data post request

--- about/test_views.py
+++ about/test_views.py
@@ -12,6 +12,7 @@ class TestAboutView(TestCase):
             title="About Me", content="This is about me.")
         self.about_content.save()
 
+
     def test_render_about_page_with_collaborate_form(self):
         """Verifies get request for about me containing a collaboration form"""
         response = self.client.get(reverse('about'))
@@ -19,3 +20,17 @@ class TestAboutView(TestCase):
         self.assertIn(b'About Me', response.content)
         self.assertIsInstance(
             response.context['collaborate_form'], CollaborateForm)
+    
+
+    def test_successful_collaboration_request(self):
+        request_data = {
+            'name': 'test',
+            'email': 'test@test.com',
+            'message': 'test',
+        }
+
+        response = self.client.post(reverse('about'), request_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Collaboration request received! I endeavour to respond within 2 working days.',
+            response.content)
